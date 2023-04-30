923df1c33a5691bd8acedbbf09e2bf92e67eff97
Akshay Udda <akshayudda955@gmail.com>
2023-04-30 16:17:12 +0530

    Completed Task3:
    	Modified Customers entity class to hold multiple mobile numbers as
    collection
    	Modified GetCustomerInfoServiceImpl to accommodate Task3 requirements
    	Added Postman collection GetCustomerWithMultipleMobileNumbers in
    src/main/java

--- src/main/java/com/avisys/cim/Customer.java
+++ src/main/java/com/avisys/cim/Customer.java
@@ -1,10 +1,17 @@
 package com.avisys.cim;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,8 +29,16 @@ public class Customer {
 	@Column(name = "LAST_NAME", nullable = false)
 	private String lastName;
 
-	@Column(name = "MOBILE_NUMBER", unique = true, nullable = false)
-	private String mobileNumber;
+//Commented during 3rd task
+//	@Column(name = "MOBILE_NUMBER", unique = true, nullable = false)
+//	private String mobileNumber;
+	
+	
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name = "mobile_numbers", joinColumns = @JoinColumn(name = "customer_id", nullable = false))
+	@Column(name = "mobile_number", length = 15)
+	private List<String> mobileNumbers = new ArrayList<String>();
+	
 
 	public Long getId() {
 		return id;
@@ -49,12 +64,23 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
-	public String getMobileNumber() {
-		return mobileNumber;
+	public List<String> getMobileNumbers() {
+		return mobileNumbers;
 	}
 
-	public void setMobileNumber(String mobileNumber) {
-		this.mobileNumber = mobileNumber;
+	public void setMobileNumbers(List<String> mobileNumbers) {
+		this.mobileNumbers = mobileNumbers;
 	}
+
+//Commented during 3rd task
+//	public String getMobileNumber() {
+//		return mobileNumber;
+//	}
+//
+//	public void setMobileNumber(String mobileNumber) {
+//		this.mobileNumber = mobileNumber;
+//	}
+	
+	
 
 }
